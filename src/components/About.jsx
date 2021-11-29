9d21ca35b5946461af0141b74326acb721c6dc37
Jrjesus18 <juniorlopez3@live.com>
2021-11-28 23:03:42 -0500

    Configuracion de ReactWebPackBabel

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/About.css';
+
+const About  = () =>{
+    return <h1>Hola mundo</h1>
+}
+
+export default  About;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+//referencia al componente about 
+import About  from './components/About';
+
+ReactDOM.render(
+    <About />, //lo que quiero mostrar
+    document.getElementById('root') // y donde lo quiero mostrar
+)
+   
+
